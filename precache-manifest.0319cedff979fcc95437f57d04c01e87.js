fa756cbde63b8561b685cd2fe0ce1fcba91aade0
red42 <mperepelitsa421@gmail.com>
2019-10-08 12:50:06 +0300

    Updates

---
+++ precache-manifest.0319cedff979fcc95437f57d04c01e87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76777299c5a69997d21fdbe2228cb3d",
+    "url": "/React-book-finder/index.html"
+  },
+  {
+    "revision": "1c839cf1b63f38da7670",
+    "url": "/React-book-finder/static/css/main.eddc03a3.chunk.css"
+  },
+  {
+    "revision": "f7842dc77a38a88cde36",
+    "url": "/React-book-finder/static/js/2.c37e57dd.chunk.js"
+  },
+  {
+    "revision": "1c839cf1b63f38da7670",
+    "url": "/React-book-finder/static/js/main.7506cc9a.chunk.js"
+  },
+  {
+    "revision": "cb18b247d7217b9c5a2d",
+    "url": "/React-book-finder/static/js/runtime~main.ebf2df60.js"
+  }
+]);
